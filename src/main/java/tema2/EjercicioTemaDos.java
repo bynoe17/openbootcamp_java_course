d58f8b45e6db104d86b090fbf2c0301710537c38
bynoe17 <ninfantes@uoc.edu>
2022-11-28 18:52:21 +0100

    Initial commit

---
+++ src/main/java/tema2/EjercicioTemaDos.java
@@ -0,0 +1,19 @@
+package tema2;
+
+public class EjercicioTemaDos {
+    public static void main(String[] args) {
+        int numero = 15;
+        long numeroLong = 1234567;
+        double numeroDouble = 0.14;
+        boolean isTrue = true;
+        String text = "És un text";
+
+        System.out.println("numero = " + numero);
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("isTrue = " + isTrue);;
+        System.out.println("text = " + text);
+
+
+    }
+}
